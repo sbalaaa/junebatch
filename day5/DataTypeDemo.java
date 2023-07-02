c3b36dafbab15a9cc8fd3e6ca64ea45d95aae94e
sbalaaa <sbalaaa@gmail.com>
2023-07-02 11:57:30 +0530

    day 5 code

---
+++ day5/DataTypeDemo.java
@@ -0,0 +1,29 @@
+
+public class DataTypeDemo {
+
+	public static void main(String[] args) {
+		int i = 10; // declation , initailatization
+		
+		int j; // declation
+		j = 10; // initailatization
+		
+		
+		String s = "Hello";
+		
+		Student ravi = new Student();
+
+	}
+
+}
+
+
+
+// 10
+// 1000
+
+
+// 10.2
+
+// F
+
+// true or false = boolean
